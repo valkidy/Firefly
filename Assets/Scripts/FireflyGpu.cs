80d77832f30f86bfe7f95a86ecd7a1cc712589f2
Kasaki <valkidy@gmail.com>
2022-04-09 13:39:30 +0800

    * Refine shader

--- Assets/Scripts/FireflyGpu.cs
+++ Assets/Scripts/FireflyGpu.cs
@@ -8,6 +8,7 @@ public class FireflyGpu : MonoBehaviour
 {
     struct Particle
     {
+        public uint ID;
         public Vector3 Position;
         public Vector3 Velocity;
         public float LifeRandom;
@@ -22,80 +23,98 @@ public class FireflyGpu : MonoBehaviour
     };
 
     [Range(0, 1)] public float TimeFlow = 0;
+
     [Range(1e-3F, 32F)] public float Frequency = 10F;
     [Range(1e-3F, 32F)] public float Amplitude = 10F;
-    [Range(0, 1)] public float ElapsedTime = 0;
+    [Range(0, 4)] public float ElapsedTime = 0;
     
     public ComputeShader KernelShader;
     public Material material;
+    public Mesh mesh;
     BulkMesh bulkMesh;
 
     ComputeBuffer Variant;
     ComputeBuffer ParticleBuffer;
-    ComputeBuffer PositionBuffer;
+    ComputeBuffer VertexBuffer;
     int BufferSize = 0;
     const int THREADS = 128;
-    Particle[] ParticleData;
-    Vector3[] PositionData;
+    // Particle[] ParticleData;
+    // Vector3[] PositionData;
 
-    static int CalcGroup(int numObjects, int numThreads)
-    {
-        // return numObjects / numThreads + Mathf.CeilToInt(numObjects % numThreads);
+    int CalcGroup(int numObjects, int numThreads)
+    {        
         return Mathf.CeilToInt((float)numObjects / numThreads);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void ConstructBulkMesh(Mesh mesh, ref BulkMesh bulkMesh, ref int bufferSize)
     {
-        var mesh = GetComponent<MeshFilter>().sharedMesh;
         Vector3[] vertices = mesh.vertices;
         int[] indices = mesh.GetIndices(0);
         Vector3[] normals = mesh.normals;
+        Vector2[] uv = mesh.uv;
 
-        BufferSize = indices.Length / 3;
-        Debug.Log(vertices.Length + ", " + indices.Length + ", " + BufferSize);
+        bufferSize = indices.Length / 3;
 
-        PositionData = new Vector3[BufferSize];
-        for (int i = 0; i < BufferSize; ++i) {
-            var v0 = vertices[indices[3 * i + 0]];
-            var v1 = vertices[indices[3 * i + 1]];
-            var v2 = vertices[indices[3 * i + 2]];
-            PositionData[i] = (v0 + v1 + v2) / 3F;
-        }
+        Vector2 uv0 = Vector2.zero, uv1 = Vector2.zero, uv2 = Vector2.zero;
+        Vector3 n0 = Vector3.zero, n1 = Vector3.zero, n2 = Vector3.zero;
 
-        bulkMesh = new BulkMesh();
-        for (int i = 0; i < BufferSize; ++i)
+        for (int i = 0; i < bufferSize; ++i)
         {
             var v0 = vertices[indices[3 * i + 0]];
             var v1 = vertices[indices[3 * i + 1]];
             var v2 = vertices[indices[3 * i + 2]];
             var vc = (v0 + v1 + v2) / 3F;
-            // var vc = Vector3.zero;
-            var n0 = normals[indices[3 * i + 0]];
-            var n1 = normals[indices[3 * i + 1]];
-            var n2 = normals[indices[3 * i + 2]];
 
-            bulkMesh.AddTriangle(
-                v0 - vc, v1 - vc, v2 - vc, 
-                Vector2.zero, Vector2.zero, Vector2.zero,
-                n0, n1, n2);
+            if (uv.Length > 0)
+            {
+                uv0 = uv[indices[3 * i + 0]];
+                uv1 = uv[indices[3 * i + 1]];
+                uv2 = uv[indices[3 * i + 2]];
+            }
+
+            if (normals.Length > 0)
+            {
+                n0 = normals[indices[3 * i + 0]];
+                n1 = normals[indices[3 * i + 1]];
+                n2 = normals[indices[3 * i + 2]];
+            }
+            bulkMesh.AddTriangle(v0 - vc, v1 - vc, v2 - vc, uv0, uv1, uv2, n0, n1, n2);
         }
         bulkMesh.Build();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulkMesh = new BulkMesh();
+
+        ConstructBulkMesh(mesh, ref bulkMesh, ref BufferSize);
+
+
+        // PositionData = new Vector3[BufferSize];
+        // for (int i = 0; i < BufferSize; ++i) {
+        //     var v0 = vertices[indices[3 * i + 0]];
+        //     var v1 = vertices[indices[3 * i + 1]];
+        //     var v2 = vertices[indices[3 * i + 2]];
+        //     PositionData[i] = (v0 + v1 + v2) / 3F;
+        // }
+
 
         // TODO: Init in compute shader
-        PositionBuffer = new ComputeBuffer(BufferSize, Marshal.SizeOf(typeof(Vector3)));
-        PositionBuffer.SetData(PositionData);
+        VertexBuffer = new ComputeBuffer(BufferSize, Marshal.SizeOf(typeof(Vector3)));
+        // PositionBuffer.SetData(PositionData);
 
         ParticleBuffer = new ComputeBuffer(BufferSize, Marshal.SizeOf(typeof(Particle)));
-        ParticleData = new Particle[BufferSize];
-        for (int i = 0; i < BufferSize; ++i)
-        {
-            ParticleData[i].LifeRandom = 0.001F;
-            ParticleData[i].Velocity = Vector3.zero;
-            ParticleData[i].Position = Vector3.one * i;
-            ParticleData[i].Time = 0;
-        }
-        ParticleBuffer.SetData(ParticleData);
+
+        // ParticleData = new Particle[BufferSize];
+        // for (int i = 0; i < BufferSize; ++i)
+        // {
+        //     ParticleData[i].LifeRandom = 0.001F;
+        //     ParticleData[i].Velocity = Vector3.zero;
+        //     ParticleData[i].Position = Vector3.one * i;
+        //     ParticleData[i].Time = 0;
+        // }
+        // ParticleBuffer.SetData(ParticleData);
 
         Variant = new ComputeBuffer(1, Marshal.SizeOf(typeof(ButterflyParticle)));
         var VariantData = new ButterflyParticle[1] { new ButterflyParticle() {
@@ -108,7 +127,7 @@ public class FireflyGpu : MonoBehaviour
 
     void OnDestroy()
     {
-        PositionBuffer.Release();
+        VertexBuffer.Release();
         ParticleBuffer.Release();
         Variant.Release();
     }
@@ -122,7 +141,7 @@ public class FireflyGpu : MonoBehaviour
         KernelShader.SetMatrix("_LocalToWorld", this.transform.localToWorldMatrix);
         KernelShader.SetBuffer(kernel, "_Variant", Variant);
         KernelShader.SetBuffer(kernel, "_ParticleBuffer", ParticleBuffer);
-        KernelShader.SetBuffer(kernel, "_PositionBuffer", PositionBuffer);
+        KernelShader.SetBuffer(kernel, "_PositionBuffer", VertexBuffer);
 
         int Groups = CalcGroup(BufferSize, THREADS);
         KernelShader.Dispatch(kernel, Groups, 1, 1);
@@ -135,9 +154,10 @@ public class FireflyGpu : MonoBehaviour
 
         material.SetInt("_NumParticles", BufferSize);
         material.SetBuffer("_ParticleBuffer", ParticleBuffer);
-        material.SetBuffer("_PositionBuffer", PositionBuffer);
+        material.SetBuffer("_VertexBuffer", VertexBuffer);
         material.SetBuffer("_Variant", Variant);
-        material.SetFloat("_ElapsedTime", ElapsedTime);        
+        material.SetFloat("_ElapsedTime", ElapsedTime);
+        material.SetFloat("_LocalTime", ElapsedTime);        
     }
     
     void Update()
--- Assets/Shaders/StandardGeometry.shader
+++ Assets/Shaders/StandardGeometry.shader
@@ -9,22 +9,23 @@
 		_LocalTime("Animation Time", Range(0, 4)) = 0.0
 		_ElapsedTime("Particle Time", Range(0, 1)) = 0.0
 	}
-		SubShader
-		{
-			Tags { "RenderType" = "Opaque" }			
-			LOD 100
-			Cull Off
 
-			Pass
-			{				
-				CGPROGRAM
-				#pragma target 4.0
-				#pragma vertex Vertex
-				#pragma geometry Geometry
-				#pragma fragment Fragment
-				// #pragma multi_compile_prepassfinal noshadowmask nodynlightmap nodirlightmap nolightmap
-				#include "StandardGeometry.cginc"
-				ENDCG
-			}			
-		}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }			
+		LOD 100
+		Cull Off
+
+		Pass
+		{				
+			CGPROGRAM
+			#pragma target 4.0
+			#pragma vertex Vertex
+			#pragma geometry Geometry
+			#pragma fragment Fragment
+			// #pragma multi_compile_prepassfinal noshadowmask nodynlightmap nodirlightmap nolightmap
+			#include "StandardGeometry.cginc"
+			ENDCG
+		}			
+	}
 }
